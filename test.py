687caa332b2d54c90faf6258487823f46b800eec
Emmanuel Leguy <Emmanuel.Leguy@univ-lille1.fr>
2016-12-14 14:58:39 +0100

    Ajout des tests de lib

---
+++ test.py
@@ -0,0 +1,7 @@
+from lib import ecrire, ecrireXFois
+
+print "-> Test ecrire: devrait ecrire une fois 'test'"
+ecrire("test")
+
+print "-> Test ecrireXFois: devrait ecrire 3 fois 'test'"
+ecrireXFois(3, "test");
