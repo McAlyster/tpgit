c6c8f7f70700c60512648107f362462391d36051
Emmanuel Leguy <Emmanuel.Leguy@univ-lille1.fr>
2016-12-14 15:23:54 +0100

    le nombre de répétitions est maintenant en paramètre

--- hello.py
+++ hello.py
@@ -1,4 +1,8 @@
+import sys
 from lib import ecrireXFois
 
+def main(argv):
+    ecrireXFois(int(argv[0]), "Hello world!")
+
 if __name__ == '__main__':
-    ecrireXFois(3, "Hello world!")
+    main(sys.argv[1:])
